c8dd0e41bfd160fa80362b8e4f0da3cf92bcbbeb
SamuLePompier <samuel.sanguina-blaise@epitech.digital>
2025-09-30 16:44:09 +0200

    fix code for the csv

--- ScrapeData/ScrapeGoalieData.py
+++ ScrapeData/ScrapeGoalieData.py
@@ -1,11 +1,14 @@
 import pandas as pd
 
+# URL de la page FBref
 url = "https://fbref.com/en/comps/Big5/2023-2024/keepers/players/2023-2024-Big-5-European-Leagues-Stats"
 
-tables = pd.read_html(url, header=1)
-
-keepers_df = tables[0]
+keepers_df = pd.read_html(url, header=1)[0]
 
 keepers_df = keepers_df.loc[:, ~keepers_df.columns.str.contains('^Unnamed')]
+
+keepers_df = keepers_df[keepers_df['Player'] != 'Player']
+
+keepers_df = keepers_df.reset_index(drop=True)
 
 keepers_df.to_csv("keepers_cleaned.csv", index=False)
